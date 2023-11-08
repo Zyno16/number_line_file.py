92c889748dedec8133be7436aa59fa6b11628d89
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-08 19:59:57 +0000

    Add files via upload

---
+++ number_line_file.py
@@ -0,0 +1,9 @@
+ 
+ #this is calculate how many line in file 
+fhand = open("parcing_extracting.py")
+count = 0
+for line in fhand:
+    count =count +1
+      
+    print("the number of line is  ",count)
+
